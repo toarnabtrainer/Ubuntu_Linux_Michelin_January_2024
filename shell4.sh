6b307c7b1c74b3dce1d34261098e9b641e7fd0c3
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-01-29 16:40:19 +0530

    Add files via upload

---
+++ shell4.sh
@@ -0,0 +1,55 @@
+# use of case-switch construct, applicable for bash shell only
+echo -n "Please enter the first number: "
+read num1
+echo -e "Please enter the second number: \c"
+read num2
+echo "M A I N  M E N U ..."
+echo "1 > A D D I T I O N  O P T I O N ..."
+echo "2 > S U B T R A C T I O N  O P T I O N ..."
+echo "3 > M U L T I P L I C A T I O N  O P T I O N ..."
+echo "4 > D I V I S I O N  O P T I O N ..."
+echo "5 > M O D U L U S  O P T I O N ..."
+echo "6 > E X P O N E N T I A T I O N  O P T I O N ..."
+echo -n "Please enter your choice number: "
+read choice_no
+case $choice_no in
+	1|"ONE"|"one"|[7-8]) echo "ADDITION Operation has been selected..."
+       result=$(( $num1 + $num2 ))
+	   echo "So the sum of $num1 and $num2 is $result..."
+       ;;   # end of individual case-switch body
+	2) echo "SUBTRACTION Operation has been selected..."
+       result=$(( num1 - num2 ))
+	   echo "So the difference of $num1 and $num2 is $result..."
+       ;;
+	3) echo "MULTIPLICATION Operation has been selected..."
+       result=$((num1*num2))
+	   echo "So the product of $num1 and $num2 is $result..."
+       ;;
+	4) echo "DIVISION Operation has been selected..."
+       result=$(( num1 / num2 ))
+	   echo "So the quotient of $num1 and $num2 is $result..."
+       ;;
+	5) echo "MODULUS Operation has been selected..."
+       result=$(( $num1 % $num2 ))
+	   echo "So the remainder of $num1 and $num2 is $result..."
+       ;;
+	6) echo "EXPONENTIATION Operation has been selected..."
+       result=$(( $num1 ** $num2 ))
+	   echo "So the result of $num1 ^ $num2 is $result..."
+       ;;
+	*) echo "INVALID Choice Number has been provided..."
+	   echo "No calculation has been accomplished..."
+       ;;
+esac
+echo "End of the program..."
+
+
+
+
+
+
+
+
+
+
+
