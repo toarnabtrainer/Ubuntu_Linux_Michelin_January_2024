3c1d897bc44dc6577702daddc49774f4246f0a8f
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-01-29 14:54:35 +0530

    Add files via upload

---
+++ shell1.sh
@@ -0,0 +1,6 @@
+# this shell script deals with echo and escape sequence characters
+# this is a comment or remark line starts with # symbol
+echo "This is the first line."
+echo -e '\nThis \tis the second line.'
+echo -e This \\nis \\tthe third line.
+	
