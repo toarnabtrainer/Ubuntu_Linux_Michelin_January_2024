1b06c6db6b999035eff04cbd23c1b398ffabc4cb
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-01-30 14:53:57 +0530

    Add files via upload

--- shell12.sh
+++ shell12.sh
@@ -7,11 +7,29 @@
 fname=$1
 if [ -e $fname ] ; then
 	echo "$fname is existing..."
-	if [ -f $fname ] ; then
+	if [ -f $fname ] ; then  # shell12.sh shell1.sh
 		echo "$fname is an ORDINARY file..."
+	r_perm=`ls -l $fname | cut -c2` # chmod u+rwx a.txt
+	w_perm=`ls -l $fname | cut -c3` # shell12.sh a.txt
+	x_perm=`ls -l $fname | cut -c4` # chmod u-rwx a.txt
+	if [ $r_perm = "r" ] ; then   
+		echo "$fname is having READ Permission ON"
+	else
+		echo "$fname is having READ Permission OFF"
+	fi
+	if [ $w_perm = "w" ] ; then
+		echo "$fname is having WRITE Permission ON"
+	else
+		echo "$fname is having WRITE Permission OFF"
+	fi
+	if [ $x_perm = "x" ] ; then
+		echo "$fname is having EXECUTE Permission ON"
+	else
+		echo "$fname is having EXCUTE Permission OFF"
+	fi
 	elif [ -L $fname ] ; then  # shell12.sh /dev/fd
 		echo "$fname is a LINK Special file..."
-	elif [ -d $fname ] ; then  # shell12.sh /dev/fd
+	elif [ -d $fname ] ; then  # shell12.sh /dev/block
 		echo "$fname is a DIRECTORY Special file..."
 	elif [ -b $fname ] ; then  # shell12.sh /dev/loop0
 		echo "$fname is a BLOCK Special file..."
@@ -19,7 +37,7 @@ if [ -e $fname ] ; then
 		echo "$fname is a CHARACTER Special file..."
 	fi
 else
-	echo "$fname is NOT existing..."
+	echo "$fname is NOT existing..."  # shell12.sh xyz.sh
 fi
 
 echo -e "\nEnd of the shell script..."
